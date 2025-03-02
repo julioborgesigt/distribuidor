c807a1a4cb09c3cf1925e1a0a6338d6ce9e215d6
julioborgesigt <julio.aparecido3@gmail.com>
2025-03-02 20:53:01 -0300

    json

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Servir arquivos estáticos da pasta "public"
