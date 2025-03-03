d2ca54910f7b3d3480dbadc5177d469957b58fa5
julioborgesigt <julio.aparecido3@gmail.com>
2025-03-03 12:48:49 -0300

    json

--- public/js/script.js
+++ public/js/script.js
@@ -161,15 +161,17 @@
     function limitChartContainerWidth() {
       const chartContainer = document.getElementById('chartContainer');
       if (window.innerWidth < 768) {
-        chartContainer.style.width = '200px';
+        chartContainer.style.setProperty('width', '320px', 'important');
+        chartContainer.style.setProperty('max-width', '320px', 'important');
       } else {
-        chartContainer.style.width = ''; // Remove a largura fixa para telas maiores
+        chartContainer.style.removeProperty('width');
+        chartContainer.style.removeProperty('max-width');
       }
     }
     
-    // Chama a função quando a página carrega e sempre que a tela for redimensionada
     document.addEventListener('DOMContentLoaded', limitChartContainerWidth);
     window.addEventListener('resize', limitChartContainerWidth);
+    
     
     
 
