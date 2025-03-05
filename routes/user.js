47c4346a0175c6ff810dd535c49fa09c6fccb992
julioborgesigt <julio.aparecido3@gmail.com>
2025-03-04 22:59:16 -0300

    json

--- routes/user.js
+++ routes/user.js
@@ -4,18 +4,19 @@ const router = express.Router();
 const userPageController = require('../controllers/userPageController');
 const userController = require('../controllers/UserController');
 const autenticarJWT = require('../middlewares/autenticarJWT');
+const autenticarAdmin = require('../middlewares/autenticarAdmin');
 
 // Aplica o middleware a todas as rotas deste módulo:
-router.use(autenticarJWT);
+router.use(autenticarAdmin);
 
 // Rota para a página do usuário
 router.get('/usuario', userPageController.getUserPage);
 
 // Rota protegida para obter os processos do usuário (ex: /processos)
-router.get('/processos', autenticarJWT, userController.listUserProcesses);
+router.get('/processos', autenticarAdmin, userController.listUserProcesses);
 
-router.post('/cumprir', autenticarJWT, userController.marcarCumprido);
-router.post('/update-observacoes', autenticarJWT, userController.updateObservacoes);
+router.post('/cumprir', autenticarAdmin, userController.marcarCumprido);
+router.post('/update-observacoes', autenticarAdmin, userController.updateObservacoes);
 
 
 
