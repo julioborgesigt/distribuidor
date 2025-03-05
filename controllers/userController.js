ddb5ef54d249af447a451b1d778c33c7768f3dfe
julioborgesigt <julio.aparecido3@gmail.com>
2025-03-05 12:46:24 -0300

    Add files via upload

---
+++ controllers/userController.js
@@ -0,0 +1,63 @@
+// /controllers/userController.js
+const { Process, User } = require('../models');
+const moment = require('moment-timezone');
+
+exports.listUserProcesses = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const processos = await Process.findAll({
+      where: { userId },
+      include: User
+    });
+    return res.json(processos);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Erro ao buscar processos' });
+  }
+};
+
+exports.marcarCumprido = async (req, res) => {
+  try {
+    const { processId, cumprido, resetReiteracoes } = req.body;
+    const process = await Process.findByPk(processId);
+    if (!process) {
+      return res.status(404).json({ error: 'Processo não encontrado' });
+    }
+    
+    // Atualiza o status "cumprido"
+    process.cumprido = cumprido;
+    
+    if (cumprido) {
+      // Registra a data/hora atual (horário de Brasília) quando marcado como cumprido
+      process.cumpridoDate = moment().tz('America/Sao_Paulo').toDate();
+      if (resetReiteracoes) {
+        process.reiteracoes = 0;
+      }
+    } else {
+      // Opcionalmente, limpe a data se desmarcado
+      process.cumpridoDate = null;
+    }
+    
+    await process.save();
+    return res.json({ message: 'Processo atualizado com sucesso' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Erro ao atualizar o processo' });
+  }
+};
+
+exports.updateObservacoes = async (req, res) => {
+  const { processId, observacoes } = req.body;
+  try {
+    const process = await Process.findByPk(processId);
+    if (!process) {
+      return res.status(404).send('Processo não encontrado.');
+    }
+    process.observacoes = observacoes;
+    await process.save();
+    res.send('Observações atualizadas com sucesso.');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao atualizar observações.');
+  }
+};
