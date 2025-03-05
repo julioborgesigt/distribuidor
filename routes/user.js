9b6749ccefdb017e799a58c206cb13f86628756d
julioborgesigt <julio.aparecido3@gmail.com>
2025-03-05 00:45:23 -0300

    json

--- routes/user.js
+++ routes/user.js
@@ -2,17 +2,19 @@
 const express = require('express');
 const router = express.Router();
 const userPageController = require('../controllers/userPageController');
-const userController = require('../controllers/UserController');
+const userController = require('../controllers/userController');
 const autenticarJWT = require('../middlewares/autenticarJWT');
+const autenticarAdmin = require('../middlewares/autenticarAdmin');
 
 
 // Rota para a página do usuário
 router.get('/usuario', userPageController.getUserPage);
 
 // Rota protegida para obter os processos do usuário (ex: /processos)
-router.get('/processos', autenticarJWT, userController.listUserProcesses);
+router.get('/processos', autenticarAdmin, userController.listUserProcesses);
 
-router.post('/cumprir', autenticarJWT, userController.marcarCumprido);
+router.post('/cumprir', autenticarAdmin, userController.marcarCumprido);
+router.post('/update-observacoes', autenticarAdmin, userController.updateObservacoes);
 
 
 
